ba94f41273b4cdda084109ecd7ac66b23a98ed0f
basra4ka <evgeha_ivanov_01@bk.ru>
2023-11-04 17:47:27 +0300

    commit

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -11,6 +11,16 @@ const Header = () =>{
                         <span id="group-info">Группа 09-151, 3 курс</span>
                     </div>
                 </div>
+
+                <div className="col-4">
+                    <div className="info-field">
+                        <span>Телефон: 8-929-720-55-22</span>
+                        <div>
+                            <a href="https://vk.com/ya_tut_babo4ka" className="link">Вконтакте</a>
+                            <a href="https://t.me/ya_tut_babo4ka" className="link">Телеграм</a>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
